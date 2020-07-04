853166326ba00fc1e70d70d31cab58d267503510
mirsella <mirsella@protonmail.com>
2020-07-04 23:29:33 +0200

    cleared commit history Sat 04 Jul 2020 11:29:33 PM CEST

---
+++ README.md
@@ -0,0 +1,29 @@
+<h1>A efficient backup is an easy backup</h1>
+this one is painfull to setup, but after it's pretty neat.</br>
+backupscript.sh loop through a json file, for the structure see the config.json.</br>
+with my config.json it's setup so i can call it like :</br>
+(b is a symlink in /bin/b to ~/.config/backupscript.sh)</br>
+`b d`</br>
+`b s d`</br>
+`b update.s`</br>
+`b t s d update.s update.d install.pkg install.script`</br>
+you can give it multiple action in one line. see the config.json file, see by yourself it's better than a bad explanation.</br>
+thoses arguments given to `backupscript.sh` without a dash `-` are in the config.json, it a "pack" of command.</br>
+</br>
+backupscript.sh itself have some options that have nothing to do with the json config. thoses can be easily tweaked in the script.</br>
+help -h :</br>
+`--curl <url>` : curl config.json from a URL</br>
+`-l path/to/file.json` : use a specific local config file</br>
+`-f` : use git push -f</br>
+`-c` : simply use git commit without the -m "backupscript $(date)". -c will open a $EDITOR window to put a commit message.</br>
+`-m "commit message"` : use git commit -m "commit message". parentheses are important !</br>
+</br>
+gitF is function in backupscript.sh. see example in config.json :</br>
+`A` = git add -A</br>
+`commit` = git commit -m "backupscript $(date)" # -m in not used if backupscript.sh is called with -c</br>
+`push` = git push (-f is backupscript.sh is called with -f) (use gitF_remote if set. can be set with -r) (same but gitF_branch and with -b)</br>
+`checkout <branch>` = git checkout</br>
+</br>
+those var are used in gitF push if set. or you can acess them in the json config.</br>
+`-r <remote>` : change gitF_remote value</br>
+`-b <branch>` : change gitF_branch</br>
---
+++ backupscript.sh
@@ -0,0 +1,149 @@
+#!/usr/bin/env bash
+CONFIG_FILE=$(dirname "$(realpath "$0")")/config.json
+
+function gitF() {
+  while true; do
+    case "$1" in
+      A) 
+        shift
+        git add -A || errorcode="$errorcode $?"
+        ;;
+      checkout)
+        shift
+        if git branch | grep -qw "$1"; then
+          git checkout "$1"
+          shift
+        else
+          echo "$1 is not a valid branch"
+          exit
+        fi
+        ;;
+      commit)
+        shift
+        if [[ -n "${gitF_commitmessage}" ]]; then
+          git commit -m "$gitF_commitmessage" || errorcode="$errorcode $?"
+        elif [[ ${gitF_commiteditor} = 1 ]]; then
+          git commit  || errorcode=$?
+        else
+          git commit -m "backupscript $(date)" || errorcode="$errorcode $?"
+        fi
+        ;;
+      push)
+        shift
+        git push ${gitF_force:-} ${gitF_remote:-} ${gitF_branch:-} || errorcode="$errorcode $?"
+        ;;
+      *) break;;
+    esac
+  done
+  return ${errorcode##* }
+}
+
+while true; do # take care of options
+  case $1 in
+    -ignorehostname) 
+      shift
+      export ignorehostname=1
+      echo 'ignoring hostname'
+      ;;
+    -f) # git push -f
+      shift
+      export gitF_force='-f'
+      echo 'using git push -f'
+      ;;
+    -c) # commit message with editor
+      shift
+      export gitF_commiteditor=1
+      ;;
+    -m) # -m "commit message"
+      shift
+      case $1 in
+        # -[a-zA-Z]|--[a-zA-Z]*|"")
+        -*|--*|"") 
+          echo 'error -m : no valid argument given. need commit message in parentheses.'
+          exit
+          ;;
+        *)
+          export gitF_commitmessage="$1"
+          shift
+          ;;
+      esac
+      ;;
+    -b) # change gitF branch
+      shift
+      case $1 in
+        -*|--*|"") 
+          echo 'error -b : no valid argument given.'
+          ;;
+        *)
+          export gitF_branch=$1
+          shift
+          echo 'change master'
+          ;;
+      esac
+      ;;
+    -r) # change gitF remote
+      shift
+      case $1 in
+        -*|--*|"") 
+          echo 'error -o : no valid argument given'
+          ;;
+        *)
+          export gitF_remote=$1
+          shift
+          echo 'change remote'
+          ;;
+      esac
+      ;;
+    --curl)
+      shift
+      cp "$CONFIG_FILE" "$CONFIG_FILE.bak"
+      case $1 in
+        -*|--*|"") 
+          REMOTE_URL=https://raw.githubusercontent.com/mirsella/backupscript/master/config.json # hardcoded default
+          ;;
+        *)
+          REMOTE_URL=$1
+          shift
+      esac
+       curl "$REMOTE_URL" -O "$CONFIG_FILE"
+      ;;
+    -l)
+      shift
+      case $1 in
+        -*|--*|"") 
+          echo 'error -l : no valid argument given'
+          ;;
+        *)
+          export CONFIG_FILE=$1
+          shift
+          ;;
+      esac
+      ;;
+    *-h*)
+      shift
+      echo -e 'help -h :
+      for more precise help go to github.com/mirsella/backupscript
+      --curl <url> : curl config.json from a URL
+      -l path/to/file.json : use a specific local config file
+      -f : use git push -f
+      -c : simply use git commit without the -m "backupscript $(date)". -c will open a $EDITOR window to put a commit message.
+      -m <commit message> : use gitF commit -m "commit message" 
+      -r <remote> : change gitF_remote value
+      -b <branch> : change gitF_branch'
+      ;;
+    *) break;;
+  esac
+done
+
+for argsloop in "$@"; do
+  for cmdloop in $(seq 0 $(($(jq ".${argsloop}[]" "$CONFIG_FILE" | wc -l)-1))); do 
+  cmd=$(jq ".${argsloop}[$cmdloop]" "$CONFIG_FILE" | sed 's/^"//; s/"$//')
+  echo -ne "  $cmd"\\r
+  if eval "$cmd"; then
+    echo -e "$(tput bold)\e[32mok$(tput sgr0)  $cmd"
+  else
+    echo -e "$(tput bold)\e[31mcode ${errorcode:-$?}$(tput sgr0)  $cmd"
+  fi
+  unset errorcode
+  done
+done
