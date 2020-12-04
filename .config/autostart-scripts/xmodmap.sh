ea3bd85fd43450cb4718940d08b548c717838809
mirsella <mirsella@protonmail.com>
2020-12-04 15:18:39 +0100

    backupscript ven. 04 déc. 2020 15:18:39 CET

--- .config/autostart-scripts/xmodmap.sh
+++ .config/autostart-scripts/xmodmap.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-# switch backspace and capslock
-# setxkbmap -option caps:backspace
-xmodmap -e "remove Lock = Caps_Lock"
-xmodmap -e "keysym BackSpace = Caps_Lock"
-xmodmap -e "keysym Caps_Lock = BackSpace"
-xmodmap -e "add Lock = Caps_Lock"
+xmodmap ~/.config/zsh/lib/xmodmap
