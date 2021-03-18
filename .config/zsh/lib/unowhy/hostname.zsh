c071954a1b1ae69b9b6e22d5b0f6661e6788f176
mirsella <mirsella@protonmail.com>
2021-03-18 14:46:22 +0100

    backupscript jeu. 18 mars 2021 14:46:22 CET

--- .config/zsh/lib/unowhy/hostname.zsh
+++ .config/zsh/lib/unowhy/hostname.zsh
@@ -29,3 +29,4 @@ export WORDCHARS=${WORDCHARS/\*\?\_\-\.\[\]\~\=\/\&\;\!\#\$\%\^\(\)\{\}\<\>}
 ([[ -f ~/.gtkrc-2.0 ]] && /bin/rm -v ~/.gtkrc-2.0) &!
 hash -d u=/run/media/mirsella
 hash -d w=/run/media/mirsella/68EE6279EE623F84/Users/Eleve
+export PATH="$PATH:.local/share/gem/ruby/2.7.0/bin"
